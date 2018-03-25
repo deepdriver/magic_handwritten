084dff960711ae8b97c3ba78a02a0550969ab426
pczhang <zhangpc123@outlook.com>
2018-03-25 21:38:16 +0800

    optimize result display

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 
 import cv2
 import numpy as np
-import matplotlib.pylab as plt
 
 import preprocessing
 from inference import Inferencer
@@ -16,7 +15,25 @@ infer.open()
 drawing = False  # true if mouse is pressed
 ix, iy = -1, -1
 reset = False
-img = np.zeros((280, 280, 1), np.uint8)
+convas = np.zeros((280, 570, 1), np.uint8)
+convas[:, 280:290] = 255
+
+img = convas[:, :280]
+result_area = convas[:, 290:]
+
+
+def draw_num(num):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(result_area, str(num), (55, 220), font, 8, (255, 255, 255), 15)
+
+
+def draw_result():
+    global reset
+    result_area[:] = 0
+    data = preprocessing.prepare_data(img)
+    num = infer.inference_once(data)
+    reset = True
+    draw_num(num)
 
 
 # mouse callback function
@@ -24,11 +41,14 @@ def draw_line(event, x, y, flags, param):
     global ix, iy, drawing, reset, img
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        if reset:
-            img[:] = 0
-            reset = False
-        drawing = True
-        ix, iy = x, y
+        if x > 290:
+            draw_result()
+        else:
+            if reset:
+                img[:] = 0
+                reset = False
+            drawing = True
+            ix, iy = x, y
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
             cv2.line(img, (ix, iy), (x, y), 255, 20)
@@ -43,14 +63,10 @@ def main():
     cv2.setMouseCallback('image', draw_line)
 
     while (1):
-        cv2.imshow('image', img)
+        cv2.imshow('image', convas)
         k = cv2.waitKey(1) & 0xFF
         if k == ord(' '):
-            # cv2.imwrite('test.jpg', img)
-            data = preprocessing.prepare_data(img)
-            num = infer.inference_once(data)
-            reset = True
-            print(num)
+            draw_result()
         elif k == ord('q'):
             break
         elif k == 27:
